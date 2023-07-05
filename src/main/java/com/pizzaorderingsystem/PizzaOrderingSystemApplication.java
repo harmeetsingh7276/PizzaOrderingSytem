b9f084eae6d471ff0b67efdc297f3b83187f0508
harmeet.gorowada <harmeet.gorowada@techbulls.com>
2023-07-05 11:52:04 +0530

    bug fix

--- src/main/java/com/pizzaorderingsystem/PizzaOrderingSystemApplication.java
+++ src/main/java/com/pizzaorderingsystem/PizzaOrderingSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PizzaOrderingSytemApplication {
+public class PizzaOrderingSystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PizzaOrderingSytemApplication.class, args);
+        SpringApplication.run(PizzaOrderingSystemApplication.class, args);
     }
 
 }
